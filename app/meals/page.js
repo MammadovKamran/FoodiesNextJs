4d029f3cd15dd11881100d7414e0176cc845902c
MammadovKamran <kamran.97.memmedov@gmail.com>
2024-03-05 17:51:34 +0400

    routing

---
+++ app/meals/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function MealsPage() {
+  return (
+    <div>
+      <h1>Meal Page</h1>
+      {/* <Link href="/meal-1">Meal 1</Link> */}
+    </div>
+  );
+}
