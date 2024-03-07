6cde83c443e893d2f6da8d78406d23c865847d09
MammadovKamran <kamran.97.memmedov@gmail.com>
2024-03-07 12:49:29 +0400

    complete main header

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,21 @@
 import Link from "next/link";
-
+import classes from "./page.module.css";
 export default function MealsPage() {
   return (
-    <div>
-      <h1>Meal Page</h1>
-      {/* <Link href="/meal-1">Meal 1</Link> */}
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals,created
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite reciepe and cook it yourself. it is easy and fun!</p>
+        <p className={classes.cta}>
+          <Link className={classes.cta} href="/meals/share">
+            Share Your Favorite Recipe
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
   );
 }
