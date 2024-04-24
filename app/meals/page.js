2fb9458b1b6e9dd64e5d5a1f4248d1721ca1069b
MammadovKamran <kamran.97.memmedov@gmail.com>
2024-04-24 17:53:47 +0400

    Suspense loading...

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,19 @@
+/** @format */
+
+import { Suspense } from "react";
+
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
 export default async function MealsPage() {
-  const meals = await getMeals();
-  return (
+  async function Meals() {
+    const meals = await getMeals();
+  
+    return <MealsGrid meals={meals} />;
+  }
+    return (
     <>
       <header className={classes.header}>
         <h1>
@@ -20,7 +28,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
